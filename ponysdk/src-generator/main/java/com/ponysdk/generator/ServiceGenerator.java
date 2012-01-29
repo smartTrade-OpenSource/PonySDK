6cb7f172dc9b1f4d02654a243a092e877dbac2c7
Luciano <luciano.broussal@gmail.com>
2012-01-29 17:36:01 +0100

    DAO import fix in generator

--- ponysdk/src-generator/main/java/com/ponysdk/generator/ServiceGenerator.java
+++ ponysdk/src-generator/main/java/com/ponysdk/generator/ServiceGenerator.java
@@ -292,7 +292,7 @@ public class ServiceGenerator extends BaseGenerator {
     private void generateDAO(Dao dao) throws Exception {
         final ClassWriter classWriter = new ClassWriter(getSrcGeneratedDirectory(), GeneratorHelper.getDAOPackage(domain), GeneratorHelper.getDAOClassName(domain));
 
-        classWriter.addExtend("com.ponysdk.hibernate.dao");
+        classWriter.addExtend("com.ponysdk.hibernate.dao.DAO");
 
         // Add static logger
         classWriter.addConstants("private static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(" + GeneratorHelper.getDAOClassName(domain) + ".class);");
