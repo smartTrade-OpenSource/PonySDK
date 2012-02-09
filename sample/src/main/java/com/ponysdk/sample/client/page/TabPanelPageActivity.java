1caeb4551a0de5346d50c4acebd14e0b7c4f925f
nicolas.ciaravola.pro@gmail.com <nicolas.ciaravola.pro@gmail.com>
2012-02-09 18:50:36 +0100

    Add TabPage Add DockPanelPage

--- sample/src/main/java/com/ponysdk/sample/client/page/TabPanelPageActivity.java
+++ sample/src/main/java/com/ponysdk/sample/client/page/TabPanelPageActivity.java
@@ -23,12 +23,11 @@
 
 package com.ponysdk.sample.client.page;
 
-import com.ponysdk.core.place.Place;
-import com.ponysdk.impl.webapplication.page.PageActivity;
 import com.ponysdk.ui.server.addon.PNotificationManager;
 import com.ponysdk.ui.server.basic.PButton;
+import com.ponysdk.ui.server.basic.PDockLayoutPanel;
 import com.ponysdk.ui.server.basic.PLabel;
-import com.ponysdk.ui.server.basic.PSimplePanel;
+import com.ponysdk.ui.server.basic.PSimpleLayoutPanel;
 import com.ponysdk.ui.server.basic.PTabPanel;
 import com.ponysdk.ui.server.basic.PVerticalPanel;
 import com.ponysdk.ui.server.basic.event.PBeforeSelectionHandler;
@@ -37,17 +36,24 @@ import com.ponysdk.ui.server.basic.event.PClickHandler;
 import com.ponysdk.ui.server.basic.event.PSelectionEvent;
 import com.ponysdk.ui.server.basic.event.PSelectionHandler;
 
-public class TabPanelPageActivity extends PageActivity {
+public class TabPanelPageActivity extends SamplePageActivity {
 
-    private final PTabPanel tabPanel = new PTabPanel();
-
-    private int tabCount = 0;
+    protected int tabCount = 0;
 
     public TabPanelPageActivity() {
-        super("TabPanel", "Fast UI Components");
-        tabPanel.setWidth("400px");
-        tabPanel.setHeight("400px");
-        tabPanel.setAnimationEnabled(true);
+        super("Tab Panel", "Panels");
+    }
+
+    @Override
+    protected void onFirstShowPage() {
+        super.onFirstShowPage();
+
+        PDockLayoutPanel dockLayoutPanel = new PDockLayoutPanel();
+        dockLayoutPanel.setSizeFull();
+
+        final PTabPanel tabPanel = new PTabPanel();
+        tabPanel.setSizeFull();
+
         tabPanel.addBeforeSelectionHandler(new PBeforeSelectionHandler<Integer>() {
 
             @Override
@@ -62,47 +68,35 @@ public class TabPanelPageActivity extends PageActivity {
                 PNotificationManager.showTrayNotification("onSelection, tab index : " + event.getSelectedItem());
             }
         });
-    }
-
-    @Override
-    protected void onInitialization() {}
-
-    @Override
-    protected void onShowPage(final Place place) {}
-
-    @Override
-    protected void onLeavingPage() {}
-
-    @Override
-    protected void onFirstShowPage() {
-        final PVerticalPanel verticalPanel = new PVerticalPanel();
-        verticalPanel.setSizeFull();
-
-        final PSimplePanel layoutPanel = new PSimplePanel();
-        layoutPanel.setStyleProperty("border", "2px solid red");
-        layoutPanel.setSizeFull();
 
         final PButton button = new PButton("Add Tab");
+        button.setWidth("100px");
         button.addClickHandler(new PClickHandler() {
 
             @Override
             public void onClick(final PClickEvent clickEvent) {
-                final PVerticalPanel tabContent = new PVerticalPanel();
-                tabContent.setSizeFull();
-
-                final int tabIndex = tabCount;
-                final PLabel label = new PLabel("content-" + tabIndex);
-                tabContent.add(label);
-                tabContent.setStyleProperty("border", "2px solid red");
-                tabPanel.add(tabContent, "Tab-" + tabIndex);
-                tabCount++;
+                addTabContent(tabPanel);
             }
         });
 
-        layoutPanel.setWidget(tabPanel);
-        verticalPanel.add(button);
-        verticalPanel.add(layoutPanel);
+        PVerticalPanel actionPanel = new PVerticalPanel();
+        actionPanel.setSpacing(10);
 
-        pageView.getBody().setWidget(verticalPanel);
+        dockLayoutPanel.addNorth(button, 30);
+        dockLayoutPanel.add(tabPanel);
+
+        addTabContent(tabPanel);
+
+        examplePanel.setWidget(dockLayoutPanel);
+    }
+
+    protected void addTabContent(final PTabPanel tabPanel) {
+        final PSimpleLayoutPanel tabContent = new PSimpleLayoutPanel();
+
+        final int tabIndex = tabCount;
+        final PLabel label = new PLabel("content-" + tabIndex);
+        tabContent.setWidget(label);
+        tabPanel.add(tabContent, "Tab-" + tabIndex);
+        tabCount++;
     }
 }
