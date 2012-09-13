6ef480404cdcf3b9635ff6d436fb0010c079d220
nciaravola <nicolas.ciaravola.pro@gmail.com>
2012-09-13 20:58:54 +0200

    First commit : PonySDK 's Website

---
+++ website/src/main/html/config.php
@@ -0,0 +1,3 @@
+<?php
+    $email_to =   'email@domain.com'; //the address to which the email will be sent
+?>
---
+++ website/src/main/html/footer.php
@@ -0,0 +1,4 @@
+<p class="grid_12 footer clearfix">
+	<span class="float"><b>&copy; Copyright</b> <a href="">PonySDK</a>.</span>
+	<a class="float right" href="#"><?php echo gmdate("Y") ?></a>
+</p>
