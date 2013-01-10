8d062355a3b8d933e921b3c6e276fa45c9fcabef
nicolas.ciaravola.pro@gmail.com <nicolas.ciaravola.pro@gmail.com>
2013-01-10 12:45:02 +0100

    fix #26 fix #27

--- ponysdk/src-core/main/java/com/ponysdk/ui/server/basic/PTwinListBox.java
+++ ponysdk/src-core/main/java/com/ponysdk/ui/server/basic/PTwinListBox.java
@@ -28,6 +28,7 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
+import com.ponysdk.impl.theme.PonySDKTheme;
 import com.ponysdk.ui.server.basic.PListBox.ListItem;
 import com.ponysdk.ui.server.basic.event.HasPChangeHandlers;
 import com.ponysdk.ui.server.basic.event.PChangeEvent;
@@ -41,39 +42,27 @@ public class PTwinListBox<T> extends PFlexTable implements HasPChangeHandlers {
     private boolean enabled = true;
 
     private final String leftCaption;
-
     private final String rightCaption;
 
     private PListBox leftListBox;
-
     private PListBox rightListBox;
 
     private PButton switchButton;
 
-    private PButton leftToRightButton;
-
-    private PButton rightToLeftButton;
-
-    private boolean multiButton;
-
     private final List<PChangeHandler> handlers = new ArrayList<PChangeHandler>();
 
     public PTwinListBox() {
-        this(null, null, false, false);
+        this(null, null, false);
     }
 
     public PTwinListBox(final String leftCaption, final String rightCaption) {
-        this(leftCaption, rightCaption, false, false);
+        this(leftCaption, rightCaption, false);
     }
 
     public PTwinListBox(final String leftCaption, final String rightCaption, final boolean containsEmptyItem) {
-        this(rightCaption, rightCaption, containsEmptyItem, false);
-    }
-
-    public PTwinListBox(final String leftCaption, final String rightCaption, final boolean containsEmptyItem, final boolean multiButton) {
+        addStyleName(PonySDKTheme.TWIN_LISTBOX);
         this.leftCaption = leftCaption;
         this.rightCaption = rightCaption;
-        this.multiButton = multiButton;
         init(containsEmptyItem);
     }
 
@@ -89,85 +78,48 @@ public class PTwinListBox<T> extends PFlexTable implements HasPChangeHandlers {
         }
 
         leftListBox = new PListBox(containsEmptyItem, true);
+        leftListBox.addStyleName(PonySDKTheme.TWIN_LISTBOX_LEFT_LISTBOX);
+
         rightListBox = new PListBox(containsEmptyItem, true);
+        rightListBox.addStyleName(PonySDKTheme.TWIN_LISTBOX_RIGHT_LISTBOX);
 
         setWidget(1, 0, leftListBox);
         setWidget(1, 2, rightListBox);
 
-        if (!multiButton) {
-            switchButton = new PButton("<>");
-            switchButton.addClickHandler(new PClickHandler() {
+        switchButton = new PButton("<>");
+        switchButton.addStyleName(PonySDKTheme.TWIN_LISTBOX_SWITCH_BUTTON);
+        switchButton.addClickHandler(new PClickHandler() {
 
-                @Override
-                public void onClick(final PClickEvent clickEvent) {
-                    final List<ListItem> leftRemovedItems = new ArrayList<ListItem>();
-                    for (int i = leftListBox.getItemCount(); i > 0; i--) {
-                        if (leftListBox.isItemSelected(i - 1)) {
-                            leftRemovedItems.add(leftListBox.removeItem(i - 1));
-                        }
+            @Override
+            public void onClick(final PClickEvent clickEvent) {
+                final List<ListItem> leftRemovedItems = new ArrayList<ListItem>();
+                for (int i = leftListBox.getItemCount(); i > 0; i--) {
+                    if (leftListBox.isItemSelected(i - 1)) {
+                        leftRemovedItems.add(leftListBox.removeItem(i - 1));
                     }
-
-                    final List<ListItem> rightRemovedItems = new ArrayList<ListItem>();
-                    for (int i = rightListBox.getItemCount(); i > 0; i--) {
-                        if (rightListBox.isItemSelected(i - 1)) {
-                            rightRemovedItems.add(rightListBox.removeItem(i - 1));
-                        }
-                    }
-
-                    for (int i = leftRemovedItems.size() - 1; i >= 0; i--) {
-                        final ListItem listItem = leftRemovedItems.get(i);
-                        rightListBox.addItem(listItem.label, listItem.value);
-                    }
-                    for (int i = rightRemovedItems.size() - 1; i >= 0; i--) {
-                        final ListItem listItem = rightRemovedItems.get(i);
-                        leftListBox.addItem(listItem.label, listItem.value);
-                    }
-
-                    fireChangeHandler();
-
                 }
-            });
-            setWidget(1, 1, switchButton);
-        } else {
-            final PFlowPanel buttonsPanel = new PFlowPanel();
-            leftToRightButton = new PButton(">");
-            leftToRightButton.addClickHandler(new PClickHandler() {
 
-                @Override
-                public void onClick(final PClickEvent clickEvent) {
-                    int start = 0;
-                    if (containsEmptyItem) {
-                        start++;
+                final List<ListItem> rightRemovedItems = new ArrayList<ListItem>();
+                for (int i = rightListBox.getItemCount(); i > 0; i--) {
+                    if (rightListBox.isItemSelected(i - 1)) {
+                        rightRemovedItems.add(rightListBox.removeItem(i - 1));
                     }
-                    for (int i = start; i < rightListBox.getItemCount(); i++) {
-                        if (rightListBox.isItemSelected(i)) {
-                            // selectValues.add(rightListBox.getItem(i));
-                        }
-                    }
-                    // refresh();
                 }
-            });
-            buttonsPanel.add(leftToRightButton);
-            rightToLeftButton = new PButton("<");
-            rightToLeftButton.addClickHandler(new PClickHandler() {
 
-                @Override
-                public void onClick(final PClickEvent clickEvent) {
-                    int start = 0;
-                    if (containsEmptyItem) {
-                        start++;
-                    }
-                    for (int i = start; i < leftListBox.getItemCount(); i++) {
-                        if (leftListBox.isItemSelected(i)) {
-                            // selectValues.remove(leftListBox.getItem(i));
-                        }
-                    }
-                    // refresh();
+                for (int i = leftRemovedItems.size() - 1; i >= 0; i--) {
+                    final ListItem listItem = leftRemovedItems.get(i);
+                    rightListBox.addItem(listItem.label, listItem.value);
                 }
-            });
-            buttonsPanel.add(rightToLeftButton);
-            setWidget(1, 1, buttonsPanel);
-        }
+                for (int i = rightRemovedItems.size() - 1; i >= 0; i--) {
+                    final ListItem listItem = rightRemovedItems.get(i);
+                    leftListBox.addItem(listItem.label, listItem.value);
+                }
+
+                fireChangeHandler();
+
+            }
+        });
+        setWidget(1, 1, switchButton);
     }
 
     protected void fireChangeHandler() {
@@ -195,12 +147,7 @@ public class PTwinListBox<T> extends PFlexTable implements HasPChangeHandlers {
         this.enabled = enabled;
         rightListBox.setEnabled(enabled);
         leftListBox.setEnabled(enabled);
-        if (multiButton) {
-            leftToRightButton.setEnabled(enabled);
-            rightToLeftButton.setEnabled(enabled);
-        } else {
-            switchButton.setEnabled(enabled);
-        }
+        switchButton.setEnabled(enabled);
     }
 
     public boolean isEnabled() {
