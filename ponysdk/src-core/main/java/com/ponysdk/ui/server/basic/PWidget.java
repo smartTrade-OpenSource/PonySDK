b64048336e1b8195f58fd55bebec300f387b858d
nvelin <nvelin@smart-trade.net>
2015-11-04 14:40:57 +0100

    FIX : Send directly the array and not an encapsulated JsonObject

--- ponysdk/src-core/main/java/com/ponysdk/ui/server/basic/PWidget.java
+++ ponysdk/src-core/main/java/com/ponysdk/ui/server/basic/PWidget.java
@@ -4,10 +4,10 @@
  *  Luciano Broussal  <luciano.broussal AT gmail.com>
  *  Mathieu Barbier   <mathieu.barbier AT gmail.com>
  *  Nicolas Ciaravola <nicolas.ciaravola.pro AT gmail.com>
- *  
+ *
  *  WebSite:
  *  http://code.google.com/p/pony-sdk/
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
@@ -497,7 +497,7 @@ public abstract class PWidget extends PObject implements IsPWidget {
                 parser.parse(Model.WINDOW_ID, window.getID());
             }
             parser.comma();
-            parser.parse(handler.asJsonObject());
+            parser.parse(Model.KEY_FILTER, handler.asJsonObject());
             parser.endObject();
         }
         return handlerRegistration;
