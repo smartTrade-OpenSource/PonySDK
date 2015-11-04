83f35a2a223032e793a10c0a1235adee2f25b37c
nvelin <nvelin@smart-trade.net>
2015-11-04 14:39:14 +0100

    DEV : Use super method

--- ponysdk/src-core/main/java/com/ponysdk/ui/server/basic/PTabLayoutPanel.java
+++ ponysdk/src-core/main/java/com/ponysdk/ui/server/basic/PTabLayoutPanel.java
@@ -4,10 +4,10 @@
  *  Luciano Broussal  <luciano.broussal AT gmail.com>
  *	Mathieu Barbier   <mathieu.barbier AT gmail.com>
  *	Nicolas Ciaravola <nicolas.ciaravola.pro AT gmail.com>
- *  
+ *
  *  WebSite:
  *  http://code.google.com/p/pony-sdk/
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
@@ -78,6 +78,7 @@ public class PTabLayoutPanel extends PComplexPanel implements HasPBeforeSelectio
     private int animationDuration;
 
     public PTabLayoutPanel() {
+        init();
         saveAddHandler(Model.HANDLER_SELECTION_HANDLER);
     }
 
@@ -95,13 +96,7 @@ public class PTabLayoutPanel extends PComplexPanel implements HasPBeforeSelectio
     }
 
     public void insert(final PWidget widget, final PWidget tabWidget, final int beforeIndex) {
-        // Detach new child.
-        widget.removeFromParent();
-
-        getChildren().insert(widget, beforeIndex);
-
-        // Adopt.
-        adopt(widget);
+        insert(widget, beforeIndex);
 
         final Parser parser = Txn.get().getTxnContext().getParser();
         parser.beginObject();
@@ -123,14 +118,13 @@ public class PTabLayoutPanel extends PComplexPanel implements HasPBeforeSelectio
         // UIContext.get().assignParentID(widget.getID(), ID);
     }
 
+    @Override
+    protected void saveAdd(final int objectID, final int parentObjectID, final Model model, final int value) {
+        // Nothing to do
+    }
+
     public void insert(final PWidget widget, final String tabText, final int beforeIndex) {
-        // Detach new child.
-        widget.removeFromParent();
-
-        getChildren().insert(widget, beforeIndex);
-
-        // Adopt.
-        adopt(widget);
+        insert(widget, beforeIndex);
 
         final Parser parser = Txn.get().getTxnContext().getParser();
         parser.beginObject();
@@ -236,7 +230,7 @@ public class PTabLayoutPanel extends PComplexPanel implements HasPBeforeSelectio
 
     /**
      * Set the duration of the animated transition between tabs.
-     * 
+     *
      * @param duration
      *            the duration in milliseconds.
      */
@@ -247,7 +241,7 @@ public class PTabLayoutPanel extends PComplexPanel implements HasPBeforeSelectio
 
     /**
      * Set whether or not transitions slide in vertically or horizontally.
-     * 
+     *
      * @param isVertical
      *            true for vertical transitions, false for horizontal
      */
