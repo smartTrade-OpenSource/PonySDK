228b6eec67ecbc3a76bb48b29ff94fbfba01c08b
nvelin <nvelin@smart-trade.net>
2016-05-25 17:46:58 +0200

    Fix Gzip handler

--- ponysdk/src/main/java/com/ponysdk/impl/main/PonySDKServer.java
+++ ponysdk/src/main/java/com/ponysdk/impl/main/PonySDKServer.java
@@ -8,14 +8,16 @@ import java.util.EnumSet;
 import javax.servlet.DispatcherType;
 import javax.servlet.http.HttpServlet;
 
+import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.HttpConfiguration;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.ServerConnector;
+import org.eclipse.jetty.server.handler.HandlerList;
+import org.eclipse.jetty.server.handler.gzip.GzipHandler;
 import org.eclipse.jetty.server.session.SessionHandler;
 import org.eclipse.jetty.servlet.FilterHolder;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
-import org.eclipse.jetty.servlets.GzipFilter;
 import org.eclipse.jetty.util.ssl.SslContextFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -30,201 +32,206 @@ import com.ponysdk.core.servlet.WebSocketServlet;
 
 public class PonySDKServer {
 
-	public static final String MAPPING_TERMINAL = "/p";
-	public static final String MAPPING_BOOTSTRAP = "/*";
-	public static final String MAPPING_WS = "/ws/*";
-	public static final String MAPPING_STREAM = "/stream";
+    public static final String MAPPING_TERMINAL = "/p";
+    public static final String MAPPING_BOOTSTRAP = "/*";
+    public static final String MAPPING_WS = "/ws/*";
+    public static final String MAPPING_STREAM = "/stream";
 
-	protected static final Logger log = LoggerFactory.getLogger(Main.class);
+    protected static final Logger log = LoggerFactory.getLogger(Main.class);
 
-	protected final Server server;
+    protected final Server server;
 
-	protected ApplicationLoader applicationLoader;
+    protected ApplicationLoader applicationLoader;
 
-	protected String host = "0.0.0.0";
-	protected int port = 80;
+    protected String host = "0.0.0.0";
+    protected int port = 80;
 
-	protected ServerConnector serverConnector;
+    protected ServerConnector serverConnector;
 
-	private boolean useSSL = true;
-	private int sslPort = 443;
-	private String sslKeyStoreFile;
-	private String sslKeyStorePassphrase;
-	private String sslKeyStoreType = "JKS";
-	private String sslTrustStoreFile;
-	private String sslTrustStorePassphrase;
-	private String sslTrustStoreType = "JKS";
-	private boolean needClientAuth = false;
-	private String[] enabledProtocols = new String[] { "TLSv1", "TLSv1.1", "TLSv1.2" };
-	private String enabledCipherSuites;
+    private boolean useSSL = true;
+    private int sslPort = 443;
+    private String sslKeyStoreFile;
+    private String sslKeyStorePassphrase;
+    private String sslKeyStoreType = "JKS";
+    private String sslTrustStoreFile;
+    private String sslTrustStorePassphrase;
+    private String sslTrustStoreType = "JKS";
+    private boolean needClientAuth = false;
+    private String[] enabledProtocols = new String[] { "TLSv1", "TLSv1.1", "TLSv1.2" };
+    private String enabledCipherSuites;
 
-	public PonySDKServer() {
-		server = new Server();
-	}
+    public PonySDKServer() {
+        server = new Server();
+    }
 
-	public void start() throws Exception {
-		serverConnector = createConnector();
-		server.addConnector(serverConnector);
+    public void start() throws Exception {
+        serverConnector = createConnector();
+        server.addConnector(serverConnector);
 
-		final ServletContextHandler context = createWebApp();
-		server.setHandler(context);
+        final ServletContextHandler context = createWebApp();
 
-		context.addEventListener(applicationLoader);
-		applicationLoader.start();
+        final GzipHandler gzip = new GzipHandler();
+        final HandlerList handlers = new HandlerList();
+        handlers.setHandlers(new Handler[] {
+                context
+        });
+        gzip.setHandler(handlers);
 
-		server.start();
+        server.setHandler(gzip);
 
-		server.join();
+        context.addEventListener(applicationLoader);
+        applicationLoader.start();
 
-		log.info("Webserver started on: " + InetAddress.getLocalHost().getHostAddress() + ":" + port);
-	}
+        server.start();
 
-	protected ServletContextHandler createWebApp() {
-		final ApplicationManagerOption applicationManagerOption = applicationLoader.getApplicationManagerOption();
-		log.info("Adding application #" + applicationManagerOption.getApplicationContextName());
+        server.join();
 
-		final ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
-		context.setContextPath("/" + applicationManagerOption.getApplicationContextName());
+        log.info("Webserver started on: " + InetAddress.getLocalHost().getHostAddress() + ":" + port);
+    }
 
-		context.addServlet(new ServletHolder(createHTTPServlet()), MAPPING_TERMINAL);
-		context.addServlet(new ServletHolder(createBootstrapServlet()), MAPPING_BOOTSTRAP);
-		context.addServlet(new ServletHolder(createStreamServiceServlet()), MAPPING_STREAM);
-		context.addServlet(new ServletHolder(createWebSocketServlet()), MAPPING_WS);
+    protected ServletContextHandler createWebApp() {
+        final ApplicationManagerOption applicationManagerOption = applicationLoader.getApplicationManagerOption();
+        log.info("Adding application #" + applicationManagerOption.getApplicationContextName());
 
-		final FilterHolder filterHolder = new FilterHolder(GzipFilter.class);
-		context.addFilter(filterHolder, "/*", EnumSet.allOf(DispatcherType.class));
+        final ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.setContextPath("/" + applicationManagerOption.getApplicationContextName());
 
-		final ServletContextFilter servletContextFilter = new ServletContextFilter();
-		context.addFilter(new FilterHolder(servletContextFilter), MAPPING_BOOTSTRAP,
-				EnumSet.of(DispatcherType.REQUEST));
+        context.addServlet(new ServletHolder(createHTTPServlet()), MAPPING_TERMINAL);
+        context.addServlet(new ServletHolder(createBootstrapServlet()), MAPPING_BOOTSTRAP);
+        context.addServlet(new ServletHolder(createStreamServiceServlet()), MAPPING_STREAM);
+        context.addServlet(new ServletHolder(createWebSocketServlet()), MAPPING_WS);
 
-		final SessionHandler sessionHandler = context.getSessionHandler();
-		sessionHandler.getSessionManager().setMaxInactiveInterval(60 * applicationManagerOption.getSessionTimeout());
-		sessionHandler.addEventListener(applicationLoader);
+        final ServletContextFilter servletContextFilter = new ServletContextFilter();
+        context.addFilter(new FilterHolder(servletContextFilter), MAPPING_BOOTSTRAP,
+                EnumSet.of(DispatcherType.REQUEST));
 
-		return context;
-	}
+        final SessionHandler sessionHandler = context.getSessionHandler();
+        sessionHandler.getSessionManager().setMaxInactiveInterval(60 * applicationManagerOption.getSessionTimeout());
+        sessionHandler.addEventListener(applicationLoader);
 
-	protected ServerConnector createConnector() {
-		final ServerConnector serverConnector;
-		if (useSSL) {
-			// HTTPS
-			URL keyStore = getClass().getResource(sslKeyStoreFile);
+        return context;
+    }
 
-			if (keyStore == null) {
-				keyStore = getClass().getClassLoader().getResource(sslKeyStoreFile);
-			}
+    protected ServerConnector createConnector() {
+        final ServerConnector serverConnector;
+        if (useSSL) {
+            // HTTPS
+            URL keyStore = getClass().getResource(sslKeyStoreFile);
 
-			if (keyStore == null)
-				throw new RuntimeException("KeyStore not found #" + sslKeyStoreFile);
-			final SslContextFactory sslContextFactory = new SslContextFactory(keyStore.toExternalForm());
-			sslContextFactory.setKeyStorePassword(sslKeyStorePassphrase);
-			sslContextFactory.setKeyStoreType(sslKeyStoreType);
-			sslContextFactory.setIncludeProtocols(enabledProtocols);
-			if (enabledCipherSuites != null)
-				sslContextFactory.setIncludeCipherSuites(enabledCipherSuites);
+            if (keyStore == null) {
+                keyStore = getClass().getClassLoader().getResource(sslKeyStoreFile);
+            }
 
-			if (needClientAuth) {
-				sslContextFactory.setNeedClientAuth(needClientAuth);
-				if (sslTrustStoreFile != null) {
-					sslContextFactory.setTrustStorePath(sslTrustStoreFile);
-					sslContextFactory.setTrustStorePassword(sslTrustStorePassphrase);
-					sslContextFactory.setTrustStoreType(sslTrustStoreType);
-				}
-			}
+            if (keyStore == null)
+                throw new RuntimeException("KeyStore not found #" + sslKeyStoreFile);
+            final SslContextFactory sslContextFactory = new SslContextFactory(keyStore.toExternalForm());
+            sslContextFactory.setKeyStorePassword(sslKeyStorePassphrase);
+            sslContextFactory.setKeyStoreType(sslKeyStoreType);
+            sslContextFactory.setIncludeProtocols(enabledProtocols);
+            if (enabledCipherSuites != null)
+                sslContextFactory.setIncludeCipherSuites(enabledCipherSuites);
 
-			final HttpConfiguration httpConfiguration = new HttpConfiguration();
-			httpConfiguration.setSecurePort(sslPort);
+            if (needClientAuth) {
+                sslContextFactory.setNeedClientAuth(needClientAuth);
+                if (sslTrustStoreFile != null) {
+                    sslContextFactory.setTrustStorePath(sslTrustStoreFile);
+                    sslContextFactory.setTrustStorePassword(sslTrustStorePassphrase);
+                    sslContextFactory.setTrustStoreType(sslTrustStoreType);
+                }
+            }
 
-			serverConnector = new ServerConnector(server, sslContextFactory);
-		} else {
-			serverConnector = new ServerConnector(server);
-		}
+            final HttpConfiguration httpConfiguration = new HttpConfiguration();
+            httpConfiguration.setSecurePort(sslPort);
 
-		serverConnector.setPort(port);
-		serverConnector.setHost(host);
-		serverConnector.setReuseAddress(true);
+            serverConnector = new ServerConnector(server, sslContextFactory);
+        } else {
+            serverConnector = new ServerConnector(server);
+        }
 
-		return serverConnector;
-	}
+        serverConnector.setPort(port);
+        serverConnector.setHost(host);
+        serverConnector.setReuseAddress(true);
 
-	protected HttpServlet createHTTPServlet() {
-		return new PonySDKHttpServlet();
-	}
+        return serverConnector;
+    }
 
-	protected BootstrapServlet createBootstrapServlet() {
-		final BootstrapServlet bootstrapServlet = new BootstrapServlet();
-		bootstrapServlet.setApplication(applicationLoader.getApplicationManagerOption());
-		return bootstrapServlet;
-	}
+    protected HttpServlet createHTTPServlet() {
+        return new PonySDKHttpServlet();
+    }
 
-	protected StreamServiceServlet createStreamServiceServlet() {
-		return new StreamServiceServlet();
-	}
+    protected BootstrapServlet createBootstrapServlet() {
+        final BootstrapServlet bootstrapServlet = new BootstrapServlet();
+        bootstrapServlet.setApplication(applicationLoader.getApplicationManagerOption());
+        return bootstrapServlet;
+    }
 
-	protected WebSocketServlet createWebSocketServlet() {
-		return new WebSocketServlet();
-	}
+    protected StreamServiceServlet createStreamServiceServlet() {
+        return new StreamServiceServlet();
+    }
 
-	public void stop() throws Exception {
-		if (server != null)
-			server.stop();
-	}
+    protected WebSocketServlet createWebSocketServlet() {
+        return new WebSocketServlet();
+    }
 
-	public void setApplicationLoader(final ApplicationLoader applicationLoader) {
-		this.applicationLoader = applicationLoader;
-	}
+    public void stop() throws Exception {
+        if (server != null)
+            server.stop();
+    }
 
-	public void setHost(final String host) {
-		this.host = host;
-	}
+    public void setApplicationLoader(final ApplicationLoader applicationLoader) {
+        this.applicationLoader = applicationLoader;
+    }
 
-	public void setPort(final int port) {
-		this.port = port;
-	}
+    public void setHost(final String host) {
+        this.host = host;
+    }
 
-	public void setUseSSL(final boolean useSSL) {
-		this.useSSL = useSSL;
-	}
+    public void setPort(final int port) {
+        this.port = port;
+    }
 
-	public void setSslKeyStoreFile(final String sslKeyStoreFile) {
-		this.sslKeyStoreFile = sslKeyStoreFile;
-	}
+    public void setUseSSL(final boolean useSSL) {
+        this.useSSL = useSSL;
+    }
 
-	public void setSslKeyStorePassphrase(final String sslKeyStorePassphrase) {
-		this.sslKeyStorePassphrase = sslKeyStorePassphrase;
-	}
+    public void setSslKeyStoreFile(final String sslKeyStoreFile) {
+        this.sslKeyStoreFile = sslKeyStoreFile;
+    }
 
-	public void setSslPort(final int sslPort) {
-		this.sslPort = sslPort;
-	}
+    public void setSslKeyStorePassphrase(final String sslKeyStorePassphrase) {
+        this.sslKeyStorePassphrase = sslKeyStorePassphrase;
+    }
 
-	public void setSslKeyStoreType(final String sslKeyStoreType) {
-		this.sslKeyStoreType = sslKeyStoreType;
-	}
+    public void setSslPort(final int sslPort) {
+        this.sslPort = sslPort;
+    }
 
-	public void setSslTrustStoreFile(final String sslTrustStoreFile) {
-		this.sslTrustStoreFile = sslTrustStoreFile;
-	}
+    public void setSslKeyStoreType(final String sslKeyStoreType) {
+        this.sslKeyStoreType = sslKeyStoreType;
+    }
 
-	public void setSslTrustStorePassphrase(final String sslTrustStorePassphrase) {
-		this.sslTrustStorePassphrase = sslTrustStorePassphrase;
-	}
+    public void setSslTrustStoreFile(final String sslTrustStoreFile) {
+        this.sslTrustStoreFile = sslTrustStoreFile;
+    }
 
-	public void setSslTrustStoreType(final String sslTrustStoreType) {
-		this.sslTrustStoreType = sslTrustStoreType;
-	}
+    public void setSslTrustStorePassphrase(final String sslTrustStorePassphrase) {
+        this.sslTrustStorePassphrase = sslTrustStorePassphrase;
+    }
 
-	public void setNeedClientAuth(final boolean needClientAuth) {
-		this.needClientAuth = needClientAuth;
-	}
+    public void setSslTrustStoreType(final String sslTrustStoreType) {
+        this.sslTrustStoreType = sslTrustStoreType;
+    }
 
-	public void setEnabledProtocols(final String[] enabledProtocols) {
-		this.enabledProtocols = enabledProtocols;
-	}
+    public void setNeedClientAuth(final boolean needClientAuth) {
+        this.needClientAuth = needClientAuth;
+    }
 
-	public void setEnabledCipherSuites(final String enabledCipherSuites) {
-		this.enabledCipherSuites = enabledCipherSuites;
-	}
+    public void setEnabledProtocols(final String[] enabledProtocols) {
+        this.enabledProtocols = enabledProtocols;
+    }
+
+    public void setEnabledCipherSuites(final String enabledCipherSuites) {
+        this.enabledCipherSuites = enabledCipherSuites;
+    }
 
 }
