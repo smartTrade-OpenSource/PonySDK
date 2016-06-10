8e6068fcfbc53dbac4cddb8c2af4030eb2cec1c1
nvelin <nvelin@smart-trade.net>
2016-06-10 17:25:24 +0200

    FIX FormField type

--- ponysdk/src/main/java/com/ponysdk/core/ui/form/formfield/FormFieldListener.java
+++ ponysdk/src/main/java/com/ponysdk/core/ui/form/formfield/FormFieldListener.java
@@ -23,12 +23,12 @@
 
 package com.ponysdk.core.ui.form.formfield;
 
-import com.ponysdk.core.ui.basic.PWidget;
+import com.ponysdk.core.ui.basic.IsPWidget;
 import com.ponysdk.core.ui.form.validator.ValidationResult;
 
 public interface FormFieldListener {
 
-    void afterReset(FormField<?, ? extends PWidget> formField);
+    void afterReset(FormField<?, ? extends IsPWidget> formField);
 
-    void afterValidation(FormField<?, ? extends PWidget> formField, ValidationResult validationResult);
+    void afterValidation(FormField<?, ? extends IsPWidget> formField, ValidationResult validationResult);
 }
