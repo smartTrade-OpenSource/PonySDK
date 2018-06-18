c618651d0a88e27c904e92c0cd856185b069ff57
Nicolas Velin <nicolas_pro@velin.fr>
2018-06-18 16:38:56 +0200

    PERF : Avoid DestroyListener leaks in UIContext

--- ponysdk/src/main/java/com/ponysdk/core/server/application/UIContext.java
+++ ponysdk/src/main/java/com/ponysdk/core/server/application/UIContext.java
@@ -23,9 +23,9 @@
 
 package com.ponysdk.core.server.application;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
@@ -103,7 +103,7 @@ public class UIContext {
 
     private final PCookies cookies = new PCookies();
 
-    private final List<ContextDestroyListener> destroyListeners = new ArrayList<>();
+    private final Set<ContextDestroyListener> destroyListeners = new HashSet<>();
     @Deprecated(forRemoval = true, since = "v2.8.0")
     private final TxnContext context;
     private final Set<DataListener> listeners = Collections.newSetFromMap(new ConcurrentHashMap<>());
@@ -698,6 +698,17 @@ public class UIContext {
      */
     public void addContextDestroyListener(final ContextDestroyListener listener) {
         destroyListeners.add(listener);
+    }
+
+    /**
+     * Removes a {@link ContextDestroyListener}
+     *
+     * @param listener
+     *            the context destroy listener
+     * @since {@link #destroy()}
+     */
+    public void removeContextDestroyListener(final ContextDestroyListener listener) {
+        destroyListeners.remove(listener);
     }
 
     /**
