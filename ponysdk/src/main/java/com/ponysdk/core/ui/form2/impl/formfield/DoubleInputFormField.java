c8cd859abf56836305d090b023a2a1cae827ac3b
Jean-Bastien Reliaud <jbreliaud@smart-trade.net>
2021-12-07 13:52:42 +0100

    Fix native error message in input number when value is not an integer

--- ponysdk/src/main/java/com/ponysdk/core/ui/form2/impl/formfield/DoubleInputFormField.java
+++ ponysdk/src/main/java/com/ponysdk/core/ui/form2/impl/formfield/DoubleInputFormField.java
@@ -1,7 +1,11 @@
+
 package com.ponysdk.core.ui.form2.impl.formfield;
 
+import com.ponysdk.core.ui.basic.PTextBox;
+
 public class DoubleInputFormField extends NumberInputFormField<Double> {
-    public DoubleInputFormField(String caption) {
+
+    public DoubleInputFormField(final String caption) {
         super(caption);
     }
 
@@ -9,5 +13,12 @@ public class DoubleInputFormField extends NumberInputFormField<Double> {
     public Double getValue() {
         if (input.getText().isEmpty()) return null;
         return Double.valueOf(input.getText());
+    }
+
+    @Override
+    protected PTextBox createInnerWidget() {
+        final PTextBox input = super.createInnerWidget();
+        input.setAttribute("step", "any");
+        return input;
     }
 }
--- ponysdk/src/main/java/com/ponysdk/core/ui/form2/impl/formfield/FloatInputFormField.java
+++ ponysdk/src/main/java/com/ponysdk/core/ui/form2/impl/formfield/FloatInputFormField.java
@@ -1,7 +1,11 @@
+
 package com.ponysdk.core.ui.form2.impl.formfield;
 
+import com.ponysdk.core.ui.basic.PTextBox;
+
 public class FloatInputFormField extends NumberInputFormField<Float> {
-    public FloatInputFormField(String caption) {
+
+    public FloatInputFormField(final String caption) {
         super(caption);
     }
 
@@ -9,5 +13,12 @@ public class FloatInputFormField extends NumberInputFormField<Float> {
     public Float getValue() {
         if (input.getText().isEmpty()) return null;
         return Float.valueOf(input.getText());
+    }
+
+    @Override
+    protected PTextBox createInnerWidget() {
+        final PTextBox input = super.createInnerWidget();
+        input.setAttribute("step", "any");
+        return input;
     }
 }
